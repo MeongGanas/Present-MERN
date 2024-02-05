027e666a6867e04a661c35101c39cf8c480cd0ab
MeongGanas <farreluken@gmail.com>
2024-02-05 21:18:25 +0800

    membuat component sidebar

---
+++ client/src/components/Sidebar.jsx
@@ -0,0 +1,60 @@
+import {
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+} from "@chakra-ui/react";
+
+import home from "../img/4836512 1.svg";
+import setting from "../img/4860084 1.svg";
+import list from "../img/5504165 1.svg";
+import { Link } from "react-router-dom";
+
+export default function Sidebar({ active }) {
+  return (
+    <div
+      className={`bg-white z-10 fixed left-0 top-0 ${
+        active ? "block w-56 translate-x-0" : "w-56 -translate-x-full"
+      } h-screen pt-16 border-r-2 border-[#D9D9D9] transition-all duration-300`}
+    >
+      <div className="border-b-2 pt-4 pb-3">
+        <Link to="/home" className="sidemenu">
+          <img src={home} alt="" width="24" />
+          <span>Home</span>
+        </Link>
+      </div>
+      <div className="py-3">
+        <Accordion allowMultiple>
+          <AccordionItem>
+            <h2>
+              <AccordionButton>
+                <div className="flex items-center gap-8 py-3 px-10 font-bold">
+                  <img src={list} alt="" width="24" />
+                  Lists
+                </div>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel pb={4}>
+              <ul className="mt-2">
+                <li>
+                  <Link to="/list1" className="sidemenu">
+                    <span className="border h-6 w-6 rounded-full border-black"></span>
+                    List 1
+                  </Link>
+                </li>
+              </ul>
+            </AccordionPanel>
+          </AccordionItem>
+        </Accordion>
+      </div>
+      <div className="border-t-2 py-3">
+        <Link to="/settings" className="sidemenu">
+          <img src={setting} alt="" width="24" />
+          <span>Settings</span>
+        </Link>
+      </div>
+    </div>
+  );
+}
