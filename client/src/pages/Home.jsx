2de907d7b670e126be0f7f395854077545e5cbbf
MeongGanas <farreluken@gmail.com>
2024-02-05 14:45:54 +0800

    memperbaiki bug height

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import NotJoin from "../ui/home/NotJoin";
 import WholeCard from "../components/Card";
 
 export default function Home() {
-  const [absentee, setAbsentee] = useState(0);
+  const [absentee, setAbsentee] = useState(null);
   return (
     <>
       {!absentee && <NotJoin />}
