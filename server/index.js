10e9e24aa8c0dc7756b64f817b2d592d8bbad0de
MeongGanas <farreluken@gmail.com>
2024-02-12 18:50:05 +0800

    tambah cors ke route api

--- server/index.js
+++ server/index.js
@@ -9,8 +9,6 @@ const cors = require("cors");
 const UserRoutes = require("./src/routes/user");
 const AbsenteeRoutes = require("./src/routes/absentee");
 
-app.use(cors());
-
 app.use(express.json());
 
 app.use((req, res, next) => {
@@ -18,8 +16,8 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use("/api/user", UserRoutes);
-app.use("/api/absentee", AbsenteeRoutes);
+app.use("/api/user", cors(), UserRoutes);
+app.use("/api/absentee", cors(), AbsenteeRoutes);
 
 mongoose
   .connect(process.env.MONGO_URL)
