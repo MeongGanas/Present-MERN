c5e1d595a670e9ac2677f24249c70402df0860d6
MeongGanas <farreluken@gmail.com>
2024-02-12 20:29:58 +0800

    debug absetee create

--- client/src/lib/actions.jsx
+++ client/src/lib/actions.jsx
@@ -34,7 +34,7 @@ export async function createAbsentee(absentName, ownerName, userId) {
 
   try {
     const response = await axios.post(
-      "/api/absentee/create" + userId.toString(),
+      `https://present-server-nine.vercel.app/api/absentee/create/${userId}`,
       {
         absentName,
         ownerName,
--- server/src/controllers/AbsenteeController.js
+++ server/src/controllers/AbsenteeController.js
@@ -20,18 +20,20 @@ const createAbsent = async (req, res) => {
 
   const code = crypto.randomBytes(6).toString("hex").slice(0, 6).toUpperCase();
 
-  try {
-    const absentee = await Absentee.create({
-      ...data,
-      userId,
-      code,
-      usersJoin: [],
-    });
+  res.status(200).json({ code, data, userId });
 
-    res.status(200).json({ absentee });
-  } catch (err) {
-    res.status(500).json({ mssg: err });
-  }
+  // try {
+  //   const absentee = await Absentee.create({
+  //     ...data,
+  //     userId,
+  //     code,
+  //     usersJoin: [],
+  //   });
+
+  //   res.status(200).json({ absentee });
+  // } catch (err) {
+  //   res.status(500).json({ mssg: err });
+  // }
 };
 
 const updateAbsent = async () => {};
